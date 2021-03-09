a7e9b99b43e45e652f8cad97f20b99464f68ae35
TsbCloud <55632589+TsbCloud@users.noreply.github.com>
2021-03-09 19:06:21 +0530

    Update FunFactory.java

--- src/FunFactory.java
+++ src/FunFactory.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class FunFactory {
     //int game_card=0;
     static String[]games= {"G1","G2","G3","G4","G5","G6","G7","G8","G9","G10"};
-    static HashSet<String> l= new HashSet();          
+   static LinkedHashSet<String> l= new LinkedHashSet();          
    //static String username1;
     //static int password1;
    static  Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
@@ -11,11 +11,11 @@ public class FunFactory {
    static  int test=6;
    static int total=0;
   static int game_Count;
-    static String s;
 //   static String username= "Gamerman123";
 //    static int password= 12345678;
-    static int balance=10;
+    static int balance=700;
     static Scanner sc= new Scanner(System.in);
+    static String s;
     
     static public void update_Balance(int x) //insert balance
     {
@@ -44,36 +44,33 @@ public class FunFactory {
        {
            System.out.print(x1+" ");
        }
-       System.out.println("Select thea games you want to play");
-       switch(games[0])
-       {
-           
-       }
-      System.out.println("\nEnter the number of games you want to play");  
-       game_Count= sc.nextInt();
-       if(game_Count<=0)
-         {
-             System.out.println("Zero games selected");
-         }
-         else if(game_Count>10)
-         {
-             System.out.println("Select a valid Game");
-         }
-         else if((game_Count>0)&& (game_Count<=10))
-         {
-             System.out.println("Games selected are");
-             for(int i=0;i<game_Count;i++)
-             {
-                 System.out.println(games[i]);
-             }
-             game_Price();
-             transaction();
-         }
+       start_Game();
+//      System.out.println("\nEnter the number of games you want to play");  
+//       game_Count= sc.nextInt();
+//       try{
+//       if(game_Count<=0)
+//         {
+//             System.out.println("Zero games selected");
+//         }
+//         else if(game_Count>10)
+//         {
+//             System.out.println("Select a valid Game");
+//         }
+//         else if((game_Count>0)&& (game_Count<=10))
+//         {
+//             start_Game();
+//             game_Price();
+//             transaction();
+//         }}
+//       catch(InputMismatchException  e)
+//               {
+//               System.out.println("Improper Input please Enter the proper names of games");
+//               }
     }
     static public void start_Game()
     {
          
-          System.out.println("Select from the following games");
+          System.out.println("\nSelect from the following games and type 'done' when finished selecting");
           for(String x1:games)
           {
               System.out.print(x1+" ");
@@ -86,12 +83,20 @@ public class FunFactory {
               {
                   break;
               }
-                  l.add(s);
+                  l.add(s); // Adding selected games to list
           }
+          if(l.size()==0)
+          {
+              System.out.println("No games selected");
+          }
+          System.out.println("The selected games are ");
+          game_Count= l.size();
          for(Object x1:l)
          {
              System.out.println(x1);
          }
+         game_Price();
+         transaction();
     }
     static public void game_Price() // calculate the price of game
     {
@@ -118,16 +123,42 @@ public class FunFactory {
                  else{
                  balance= balance-total;
                  System.out.println("Current Balance is "+balance);
+                 System.out.println("Game is about to start ");
              }
     }
     public static void main(String[]ar)
     {
         System.out.println("Welcome to FunFactory");
         System.out.println("Please Enter your card");
-//        System.out.println("Enter username");
-//        username1= sc.nextLine();
-//        System.out.println("Enter password");
-//        password1= sc.nextInt();
+//        System.out.println("Type 'Enter' to continue or 'NO' to cancel");
+//        String temp;
+//        temp=sc.nextLine();
+        int i=0;
+        while(i<4)
+        { 
+            if(i==3)
+            { System.out.println("Restart game 3 wrong tries");
+                System.exit(0);}
+          System.out.println("Type 'Enter' to continue or 'NO' to cancel");
+        String temp;
+        temp=sc.nextLine();
+        if(temp.equalsIgnoreCase("Enter"))
+        {
+            System.out.println("Welcome");
+            break;
+        }
+        else if(temp.equalsIgnoreCase("No"))
+        {
+            System.out.println("Not starting the game");
+            System.exit(0);
+        }
+        else
+        {
+            System.out.println("Wrong Input, Please enter one of the below inputs");
+            i++;
+        }
+        }   
+
         System.out.println("If you want to check balance then type 'balance' ");
         System.out.println("If you want to update balance then type 'update' ");
         System.out.println("If you are ready to play game then type 'play' ");
@@ -145,6 +176,10 @@ public class FunFactory {
         else if(s1.equalsIgnoreCase("play"))
         {
             play_Game();
+        }
+        else
+        {
+            System.out.println("Please enter one of the above to continue ");
         }
     
 }}
